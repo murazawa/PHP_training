96d8c7cf8feef78c5d8dd24da1a79ed801146464
murazawa <mura.hi0115ki@gmail.com>
2022-01-14 10:49:54 +0900

    folder nameshange

---
+++ training/views/home.php
@@ -0,0 +1,19 @@
+<?php
+  require('../app/functions.php');
+  include('../app/_parts/_header.php');
+
+?>
+
+<p>CSVをデータベースに登録します</p>
+
+<form action="#" method="post">
+  <p><input type="file" name="csv_file"/></p>
+  <!--hiddenあとで-->
+  <p><button>アップロード</button></p>
+</form>
+
+array_diff_assoc
+
+
+<?php
+  include('../app/_parts/_footer.php');
