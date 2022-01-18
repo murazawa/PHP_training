1abe0abf600af62939a976973f94afb2ba1e8f43
murazawa <mura.hi0115ki@gmail.com>
2022-01-18 09:51:46 +0900

    PHP　課題

--- training/views/results.php
+++ training/views/results.php
@@ -3,10 +3,10 @@ ini_set('display_errors', "On");
 // require ('../app/functions.php');
 // require ('../app/validate.php');
 
-try
+  try
   {
-    $PDO = new PDO('mysql:host=localhost;dbname=myapp;charset=utf8','root'); // XAMPP環境
-    // $PDO = new PDO('mysql:host=localhost:8889;dbname=myapp;charset=utf8','root','root'); // MAMP環境
+    // $PDO = new PDO('mysql:host=localhost;dbname=myapp;charset=utf8','root'); // XAMPP環境
+    $PDO = new PDO('mysql:host=localhost:8889;dbname=myapp;charset=utf8','root','root'); // MAMP環境
 
     if (isset($_FILES["csvfile"]["tmp_name"]))
     {
@@ -19,21 +19,21 @@ try
       } else {
           $fp = fopen($file_tmp_name, "r");
 
-              // 配列に変換する
+          // 配列に変換する
           $csv_data = [];
           while (($data = fgetcsv($fp)) !== FALSE)
           {
             $csv_data[] = $data;
           }
-
           fclose($fp);
-        }
+      }
     } else {
-        $err_msg = "ファイルが選択されていません。";
+      $err_msg = "ファイルが選択されていません。";
     }
 
     echo 'DBに接続したよresult.php</br>';
     echo $err_msg;
   } catch  (PDOException $e) {
-      echo 'DB接続エラー:'.$e->getMessage();
+    echo 'DB接続エラー:'.$e->getMessage();
   }
+
