d1b43676c81a1d760e9f17b31ed62abd34657cde
murazawa <mura.hi0115ki@gmail.com>
2022-01-19 15:18:18 +0900

    PHP 研修 DB追加

--- training/views/results.php
+++ training/views/results.php
@@ -1,10 +1,11 @@
 <?php
 ini_set('display_errors', "On");
-// require ('../app/functions.php');
-// require ('../app/validate.php');
 
   try
   {
+    // require ('../app/functions.php');
+    // require ('../app/validate.php');
+    include('../app/_parts/_header.php');
     $PDO = new PDO('mysql:host=localhost;dbname=myapp;charset=utf8','root'); // XAMPP環境
     // $PDO = new PDO('mysql:host=localhost:8889;dbname=myapp;charset=utf8','root','root'); // MAMP環境
 
@@ -15,46 +16,29 @@ ini_set('display_errors', "On");
 
       if (pathinfo($file_name, PATHINFO_EXTENSION) != 'csv')
       {
-        $err_msg = 'CSVのみ対応しています';
+        $err_msg = '選択ファイルには、CSVタイプのファイルを指定してください。';
       } else {
           $fp = fopen($file_tmp_name, "r");
-
-          // 配列に変換する
-          $csv_data = [];
+          $flag = true;
           while (($data = fgetcsv($fp)) !== FALSE)
           {
-            $csv_data[] = $data;
-            // var_dump($data);
-            // var_dump($csv_data);
+            if($flag) { $flag = false; continue; }
+
+            $sql = 'INSERT INTO phpcsv (name, age, gender) VALUES (?, ?, ?)';
+            $stmt = $PDO->prepare($sql);
+            $stmt->bindParam(1, $data[1], PDO::PARAM_STR);
+            $stmt->bindParam(2, $data[2], PDO::PARAM_STR);
+            $stmt->bindParam(3, $data[3], PDO::PARAM_STR);
+            $stmt->execute();
           }
-          fclose($fp);
-          
-          $sql = 'INSERT INTO phpcsv (name, age, gender) VALUES (?, ?, ?)';
-          $stmt = $PDO->prepare($sql);
-          // $stmt->bindParam(1, $csv_data['id'], PDO::PARAM_INT);
-          $stmt->bindParam(1, $csv_data['name'], PDO::PARAM_STR);
-          $stmt->bindParam(2, $csv_data['age'], PDO::PARAM_INT);
-          $stmt->bindParam(3, $csv_data['gender'], PDO::PARAM_STR, 3);
-          var_dump($csv_data);
-          $stmt->execute();
-          
+
+          fclose($fp);          
       }
     } else {
-      $err_msg = "ファイルが選択されていません。";
+      $err_msg = "ファイルを選択してください。";
     }
-
-    // $sql = 'INSERT INTO phpcsv ("name", "age", "gender") VALUES (?, ?, ?)';
-    // $stmt = $PDO->prepare($sql);
-  
-
-    // // var_dump($stmt);
-    // $stmt->execute();
-
-
+    echo $err_msg;
     echo 'DBに接続したよresult.php</br>';
   } catch  (PDOException $e) {
     echo 'DB接続エラー:'.$e->getMessage();
   }
-  
-////////////////////////////////
-
