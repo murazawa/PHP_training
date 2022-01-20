76ddb5dfb484a8032e629b55b43dfd0987123ff6
murazawa <mura.hi0115ki@gmail.com>
2022-01-20 11:21:55 +0900

    PHP 課題 バリデーションチェック完了

--- training/views/results.php
+++ training/views/results.php
@@ -3,18 +3,23 @@ ini_set('display_errors', "On");
 
   try
   {
-    // require ('../app/functions.php');
-    // require ('../app/validate.php');
-    include('../app/_parts/_header.php');
     $PDO = new PDO('mysql:host=localhost;dbname=myapp;charset=utf8','root'); // XAMPP環境
     // $PDO = new PDO('mysql:host=localhost:8889;dbname=myapp;charset=utf8','root','root'); // MAMP環境
+
+    include('../app/_parts/_header.php');
+    // require ('../app/functions.php');
+    // require ('../app/validate.php');
+    
+    // トランザクション処理
+    $PDO->beginTransaction();
+
     $err_msg= "";
 
     if (!empty($_FILES["csvfile"]["tmp_name"]))
     {
       $file_tmp_name = $_FILES["csvfile"]["tmp_name"];
       $file_name = $_FILES["csvfile"]["name"];
-      
+
 
       if (pathinfo($file_name, PATHINFO_EXTENSION) != 'csv')
       {
@@ -27,22 +32,19 @@ ini_set('display_errors', "On");
 
           $flag = true;
           while (($data = fgetcsv($fp)) !== FALSE)
-          
+
           {
             // var_dump($data);
             /// キーチェック ///////////
-            if (isset($data[1]) == "" || isset($data[2]) == "")
-            { 
-              die('名前、年齢は必須項目です。');
-              // 何もなかったことにする
-              // case?使う
+            if ($data[1] == "" || $data[2] == "")
+            {
+              exit ('名前、年齢は必須項目です。');
             }
             //////////////////////////
-            
             /// フォーマットチェック ///
             if (isset($data[4]))
             {
-              $err_msg = '正しいフォーマットで入力してください。';
+              exit ('正しいフォーマットで入力してください。');
             }
             //////////////////////////
               if($flag) { $flag = false; continue; }
@@ -54,8 +56,9 @@ ini_set('display_errors', "On");
               $stmt->bindParam(3, $data[3], PDO::PARAM_STR);
               $stmt->execute();
           }
-
-          fclose($fp);          
+          
+          fclose($fp);  
+          echo 'アップロードに成功しました。';       
       }
     } else {
       $err_msg = 'ファイルを選択してください。';
@@ -63,6 +66,8 @@ ini_set('display_errors', "On");
 
     echo $err_msg;
     echo 'DBに接続したよresult.php</br>';
+    $PDO->commit();
   } catch  (PDOException $e) {
+    $PDO->rollback();
     echo 'DB接続エラー:'.$e->getMessage();
   }
