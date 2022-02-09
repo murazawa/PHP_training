3773a40e9c10d6d387a691856c89eedb1ad62607
murazawa <mura.hi0115ki@gmail.com>
2022-02-10 01:24:15 +0900

    PHP 編集機能

--- training/app/functions.php
+++ training/app/functions.php
@@ -6,3 +6,15 @@ function h($string) {
 
 
 
+function createToken() {
+  if ( !isset($_SESSION['token'])) {
+    $_SESSION['token'] = bin2hex(random_bytes(32));
+  }
+}
+
+function validateToken() {
+  if (empty($_SESSION['token']) || $_SESSION['token'] !== filter_input(INPUT_POST, 'token')) {
+    exit('おっとっと');
+  }
+}
+session_start();
--- training/views/insert.php
+++ training/views/insert.php
@@ -1,6 +1,20 @@
+<?php
+
+  ini_set('display_errors', "On");
+  include('../app/_parts/_header.php');
+  require('../app/functions.php');
+  createToken();
+
+  if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+    validateToken();
+  }
+?>
+
+
 <h1>インサート画面だよ</h1>
 
 <form action="results.php" method="post">
+  <input type="hidden" name="token" value="<? h($_SESSION['token']); ?>">
   名前:</br>
   <input type="text" name="name" id="name" value=""></br>
   年齢:</br>
