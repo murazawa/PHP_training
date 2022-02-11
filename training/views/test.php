ed144decd4fdc06e78c008c2d791a580c8963b7b
murazawa <mura.hi0115ki@gmail.com>
2022-02-11 17:18:12 +0900

    整理

--- training/views/test.php
+++ training/views/test.php
@@ -8,17 +8,10 @@ try {
   $sql = 'UPDATE phpcsv SET name = :name, age = :age, gender = :gender WHERE id = :id';
   $stmt = $PDO->prepare($sql);
 
-  $params = array(':name' => $_POST['name'], ':age' => $_POST['age'], ':gender' => $_POST['gender'], ':id' => $_POST['id']);
-  $stmt->execute($params);
 
 
 
-  echo '更新完了しました';
-
-
-
-
-  echo '接続したよ';
+  echo 'DB接続したよ';
 } catch (PDOException $e) {
           echo 'エラーが発生しました。:' . $e->getMessage();
 }
